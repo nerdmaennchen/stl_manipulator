346c5aa1d6a77e4fda484ce1a0d90c816519d389
Lutz <lutz@gottliebtfreitag.de>
2021-01-23 16:05:37 +0100

    don't double define the program'S arguments

--- src/main.cpp
+++ src/main.cpp
@@ -11,32 +11,6 @@ namespace {
 
 sargp::Parameter<std::optional<std::string>> printHelp{{}, "help", "print this help add a string which will be used in a grep-like search through the parameters"};
 
-
-arma::mat44 transform = arma::eye(4,4);
-
-sargp::Parameter<double> rotX {1., "rotX", "how much to rotate around the x axis (degrees)", []{ transform = kinematicTree::utils::getTransform(0, 0, 0, *rotX * M_PI/180., 0, 0) * transform; }};
-sargp::Parameter<double> rotY {1., "rotY", "how much to rotate around the y axis (degrees)", []{ transform = kinematicTree::utils::getTransform(0, 0, 0, 0, *rotY * M_PI/180., 0) * transform; }};
-sargp::Parameter<double> rotZ {1., "rotZ", "how much to rotate around the z axis (degrees)", []{ transform = kinematicTree::utils::getTransform(0, 0, 0, 0, 0, *rotZ * M_PI/180.) * transform; }};
-
-sargp::Parameter<double> movX {1., "movX", "how much to move along the x axis (degrees)", []{ transform = kinematicTree::utils::getTransform(*movX, 0, 0, 0, 0, 0) * transform; }};
-sargp::Parameter<double> movY {1., "movY", "how much to move along the y axis (degrees)", []{ transform = kinematicTree::utils::getTransform(0, *movY, 0, 0, 0, 0) * transform; }};
-sargp::Parameter<double> movZ {1., "movZ", "how much to move along the z axis (degrees)", []{ transform = kinematicTree::utils::getTransform(0, 0, *movZ, 0, 0, 0) * transform; }};
-
-sargp::Parameter<double> scale {1., "scale", "scale the STL", []{ transform = arma::mat44{{*scale, 0, 0, 0}, {0, *scale, 0, 0}, {0, 0, *scale, 0}, {0, 0, 0, 1}} * transform;}};
-
-sargp::Parameter<double> scaleX {1., "scaleX", "scale the x components of the STL", []{ transform = arma::mat44{{*scaleX, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}} * transform;}};
-sargp::Parameter<double> scaleY {1., "scaleY", "scale the y components of the STL", []{ transform = arma::mat44{{1, 0, 0, 0}, {0, *scaleY, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}} * transform;}};
-sargp::Parameter<double> scaleZ {1., "scaleZ", "scale the z components of the STL", []{ transform = arma::mat44{{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, *scaleZ, 0}, {0, 0, 0, 1}} * transform;}};
-
-sargp::Parameter<sargp::File> inFile  {"", "in", "the file to read from"};
-sargp::Parameter<sargp::File> outFile {"", "out", "the file to write to"};
-
-sargp::Flag invertVertexFanning {"invert_vertex_fanning", "reorder the vertices of all facets"};
-sargp::Flag addInvertedVerteces {"add_inverted_vertices", "add reordered vertices of all facets (effectively copies all vertives once)"};
-
-sargp::Parameter<double> totalMass {0., "total_mass", "total mass of the object (used to calculate mass properties)"};
-sargp::Parameter<double> density {0., "density", "density of the object (used to calculate mass properties)"};
-
 }
 
 int main(int argc, char** argv)
